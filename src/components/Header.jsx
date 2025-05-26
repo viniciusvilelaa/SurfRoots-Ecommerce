a717c5f5b0d4520bc5f9ee69a032e1f59913bc52
viniciusvilelaa <ninobez@icloud.com>
2025-05-26 16:05:32 -0300

    Fix: Finos ajustes dos nomes da classe do header, adicionado icone de conta e aumento do texto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import react from 'react';
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { RiAccountCircleLine } from "react-icons/ri";
 
 const Header = () => {
     return (
         <div className={"header-container"}>
-            <h1 className={"logo"}>Make for It</h1>
-            <div className="header__center">
-                <a href="#" className="header__link">Home</a>
-                <a href="#" className="header__link">Contact</a>
-                <a href="#" className="header__link">About</a>
-                <a href="#" className="header__link sign">Sign Up</a>
+            <h1 className={"logo"}>SURF ROOTS</h1>
+            <div className="header--center">
+                <a href="#" className="header--link">Contact</a>
+                <a href="#" className="header--link">About</a>
+                <a href="#" className="header--link sign">Sign Up</a>
             </div>
-            <div className="header__right">
-                <input type="text" className={"header__input"} />
+            <div className="header--right">
+                <input type="text" className={"header--input"} />
                 <AiOutlineShoppingCart />
+                <RiAccountCircleLine />
             </div>
         </div>
     )
