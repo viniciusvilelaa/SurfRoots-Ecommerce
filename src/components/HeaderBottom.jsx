53b214b9eb91fe0abc88de35f3da8f25207d6554
viniciusvilelaa <ninobez@icloud.com>
2025-06-16 10:57:49 -0300

    Iniciando implementacao do dropdown menu

--- src/components/HeaderBottom.jsx
+++ src/components/HeaderBottom.jsx
@@ -1,21 +1,25 @@
 import React, {useState} from 'react'
-
+import { GiSurfBoard } from "react-icons/gi";
+import { IoPricetagOutline } from "react-icons/io5";
+import { FaTshirt } from "react-icons/fa";
 
 function HeaderBottom() {
     
     return (
     <div className='navbar--bottom'>
         <ul className='navbar--bottom-container'>
-            <li>pranchas</li>
-            <li>pranchas</li>
-            <li>pranchas</li>
-            <li>pranchas</li>
-            <li>pranchas</li>
-            <li>pranchas</li>
-            <li>pranchas</li>
-            <li>pranchas</li>
-            <li>pranchas</li>
-            <li>pranchas</li>
+            <li className='dropdown-item'>
+                <GiSurfBoard className='dropdown--icon' />
+                <p>Pranchas</p>
+            </li>
+            <li className='dropdown-item'>
+                <FaTshirt className='dropdown--icon' />
+                <p>Vestuário</p>
+            </li>
+            <li className='dropdown-item'>
+                <IoPricetagOutline className='dropdown--icon' />
+                <p>Ofertas</p>
+            </li>
         </ul>
     </div>
   )
